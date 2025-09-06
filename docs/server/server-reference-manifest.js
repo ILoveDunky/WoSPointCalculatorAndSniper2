8e48464cf5baa063f5b50ecd98a74e7ad2559efe
skibidi sigma <sigmatoilet12skibidi@gmail.com>
2025-09-06 02:33:38 +0000

    Build site to docs folder for GitHub Pages

--- docs/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+n64WvFn+lgBOjiABXXKL+xAWBgXe8dEhFuV41cwSJw=\"\n}"
--- docs/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_de63eafd._.js",
-  "static/chunks/node_modules_a62ce72b._.js"
-],
-    source: "dynamic"
-});
